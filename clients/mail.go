11efa9b4a05ab8d75813a665bc65ec7fe3768af9
Vladimir Stolyarov <xakep6666@gmail.com>
2017-12-01 13:09:02 +0300

    Write mail service client

---
+++ clients/mail.go
@@ -0,0 +1,36 @@
+package clients
+
+import (
+	"git.containerum.net/ch/mail-templater/upstreams"
+	"github.com/sirupsen/logrus"
+	"gopkg.in/resty.v1"
+)
+
+type MailClient struct {
+	rest *resty.Client
+	log  *logrus.Logger
+}
+
+func NewMailClient(serverUrl string) *MailClient {
+	log := logrus.New().WithField("component", "mail_client").Logger
+	client := resty.New().SetHostURL(serverUrl).SetLogger(log.Writer())
+	return &MailClient{
+		rest: client,
+		log:  log,
+	}
+}
+
+func (mc *MailClient) SendConfirmationMail(recipient *upstreams.Recipient) error {
+	mc.log.Info("Sending confirmation mail to", recipient.Email)
+	req := &upstreams.SendRequest{}
+	req.Delay = 0
+	req.Message.Recipients = append(req.Message.Recipients, *recipient)
+	_, err := mc.rest.R().
+		SetBody(req).
+		SetResult(&upstreams.SendResponse{}).
+		Post("/templates/confirm_reg")
+	if err != nil {
+		return err
+	}
+	return nil
+}
