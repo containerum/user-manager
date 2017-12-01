7ef38f37bc1ea60eb6af2ac9337e245fcfed2e5c
Vladimir Stolyarov <xakep6666@gmail.com>
2017-12-01 16:02:40 +0300

    Add methods for token

--- models/token.go
+++ models/token.go
@@ -9,3 +9,25 @@ type Token struct {
 	IsActive  bool
 	SessionID string `gorm:"type:uuid"`
 }
+
+func (db *DB) GetUserByToken(token string) (*User, error) {
+	db.log.Debug("Get user by token", token)
+	var user User
+	resp := db.conn.Where("token = ? AND is_active = true").First(&user)
+	if resp.RecordNotFound() {
+		return nil, nil
+	}
+	return &user, resp.Error
+}
+
+func (db *DB) CreateToken(user *User, sessionID string) (*Token, error) {
+	db.log.Debug("Generate one-time token for", user.Login)
+	ret := &Token{
+		Token:     "token", // TODO: token generation here
+		User:      *user,
+		CreatedAt: time.Now().UTC(),
+		IsActive:  true,
+		SessionID: sessionID,
+	}
+	return ret, db.conn.Create(ret).Error
+}
