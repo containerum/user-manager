a39a96948527e7aa760004dd20b00ce0227e9812
Vladimir Stolyarov <xakep6666@gmail.com>
2017-12-13 14:00:45 +0300

    Add app runner

---
+++ config.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"errors"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+func setupLogger() error {
+	switch gin.Mode() {
+	case gin.TestMode, gin.DebugMode:
+	case gin.ReleaseMode:
+		logrus.SetFormatter(&logrus.JSONFormatter{})
+	}
+	viper.SetDefault("log_level", logrus.InfoLevel)
+	level := logrus.Level(viper.GetInt("log_level"))
+	if level > logrus.DebugLevel || level < logrus.PanicLevel {
+		return errors.New("invalid log level")
+	}
+	return nil
+}
+
+func getListenAddr() string {
+	viper.SetDefault("listen_addr", ":8111")
+	return viper.GetString("listen_addr")
+}
