ec643cef36d5c326fbb146f40258f942582289c0
Vladimir Stolyarov <xakep6666@gmail.com>
2017-12-26 18:59:48 +0300

    update json-types

--- vendor/git.containerum.net/ch/json-types/user-manager/types.go
+++ vendor/git.containerum.net/ch/json-types/user-manager/types.go
@@ -40,7 +40,7 @@ const (
 )
 
 type BasicLoginRequest struct {
-	Username  string `json:"login" binding:"username,email"`
+	Username  string `json:"login" binding:"required,email"`
 	Password  string `json:"password" binding:"required"`
 	ReCaptcha string `json:"recaptcha" binding:"required"`
 }
