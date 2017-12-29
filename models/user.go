651a8a3c92db967354a017c6a760152bfb5e84e0
Vladimir Stolyarov <xakep6666@gmail.com>
2017-12-29 13:23:28 +0300

    initialise response in GetBlacklistedUsers to return non-nil value

--- models/user.go
+++ models/user.go
@@ -78,7 +78,7 @@ func (db *DB) UpdateUser(user *User) error {
 
 func (db *DB) GetBlacklistedUsers(perPage, page int) ([]User, error) {
 	db.log.Debugln("Get blacklisted users")
-	var resp []User
+	resp := make([]User, 0)
 	rows, err := db.qLog.Queryx("SELECT "+userQueryColumns+" FROM users WHERE is_in_blacklist LIMIT $1 OFFSET $2",
 		perPage, page)
 	if err != nil {
