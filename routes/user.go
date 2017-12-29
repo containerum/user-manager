057a8fd5b1b8dbae0bd2b76bbf107b4791c25703
Vladimir Stolyarov <xakep6666@gmail.com>
2017-12-29 13:22:32 +0300

    add pagination to blacklistGetHandler

--- routes/user.go
+++ routes/user.go
@@ -307,7 +307,14 @@ func userToBlacklistHandler(ctx *gin.Context) {
 }
 
 func blacklistGetHandler(ctx *gin.Context) {
-	blacklisted, err := svc.DB.GetBlacklistedUsers()
+	var params umtypes.UserListQuery
+	if err := ctx.ShouldBindQuery(&params); err != nil {
+		ctx.Error(err)
+		ctx.AbortWithStatusJSON(http.StatusInternalServerError, errors.New(err.Error()))
+		return
+	}
+
+	blacklisted, err := svc.DB.GetBlacklistedUsers(params.PerPage, params.Page)
 	if err != nil {
 		ctx.Error(err)
 		ctx.AbortWithStatus(http.StatusInternalServerError)
