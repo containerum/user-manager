da6a3a1e81b559623a9cc4a1e061a1fbf052e427
Ilia Donchenko <iliad@me.com>
2018-01-29 11:17:25 +0300

    Refactor completely deleted users

---
+++ utils/random.go
@@ -0,0 +1,39 @@
+package utils
+
+import (
+	"crypto/rand"
+)
+
+const (
+	letterBytes   = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890" // 62 possibilities
+	letterIdxBits = 6                                                                // 6 bits to represent 64 possibilities / indexes
+	letterIdxMask = 1<<letterIdxBits - 1                                             // All 1-bits, as many as letterIdxBits
+)
+
+//SecureRandomString securly generates random string
+func SecureRandomString(length int) (res string, err error) {
+
+	result := make([]byte, length)
+	bufferSize := int(float64(length) * 1.3)
+	for i, j, randomBytes := 0, 0, []byte{}; i < length; j++ {
+		if j%bufferSize == 0 {
+			randomBytes, err = SecureRandomBytes(bufferSize)
+		}
+		if idx := int(randomBytes[j%length] & letterIdxMask); idx < len(letterBytes) {
+			result[i] = letterBytes[idx]
+			i++
+		}
+	}
+	res = string(result)
+	return res, nil
+}
+
+// SecureRandomBytes returns the requested number of bytes using crypto/rand
+func SecureRandomBytes(length int) ([]byte, error) {
+	var randomBytes = make([]byte, length)
+	_, err := rand.Read(randomBytes)
+	if err != nil {
+		return nil, err
+	}
+	return randomBytes, nil
+}
